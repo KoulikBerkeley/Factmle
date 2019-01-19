326db0868aed2ca1110af7573564ff10766d2067
KoulikBerkeley <koulik@berkeley.edu>
2019-01-19 11:59:36 -0800

    file not needed, code snippet is in FA_blk

--- get_block_diagonal_indices.R
+++
@@ -1,6 +0,0 @@
-# script for deducing the vectos blk_start and blk_end from number of blocks and dimension.
-
-library(ramify)
-
-blk_start = linspace(1,pp - pp/blk_number + 1, blk_number);
-blk_end =  linspace(pp/blk_number, pp, blk_number)
